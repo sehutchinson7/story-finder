e110a2f9235efa5a11a16f9a41bd59156248eb3a
Sara <shutchinson@esri.com>
2019-07-16 17:03:02 -0700

    Returning data

--- src/App.js
+++ src/App.js
@@ -8,68 +8,52 @@ class App extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      results: []
+      items: []
     };
   }
   //https://esri.github.io/arcgis-rest-js/guides/node/
   //https://esri.github.io/arcgis-rest-js/api/portal/searchItems/
 
   componentDidMount() {
-    searchItems({
-      query: `type:storymap AND typekeywords:(smstatuspublished) AND orgid:(NOT(dGkWQRTbdsLW3GnN AND 4E2kXnaVf5dNC6X0 AND nzS0F0zdNLvs7nc8))`,
-      start: 0,
-      num: 23
-    }).then(
-      results => {
-        console.log(results);
-        this.setState({
-          isLoaded: true,
-          items: results.items
-        });
-      },
-      error => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    );
+    const url = `https://www.arcgis.com/sharing/rest/search?q=type%3Astorymap%20AND%20typekeywords%3A(smstatuspublished)%20AND%20orgid%3A(NOT(dGkWQRTbdsLW3GnN%20AND%204E2kXnaVf5dNC6X0%20AND%20nzS0F0zdNLvs7nc8))&num=100&f=pjson`;
+    fetch(url)
+      .then(response => response.json())
+      .then(
+        data => {
+          console.log(data);
+          this.setState({
+            isLoaded: true,
+            current: data,
+            items: data.results
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
   }
 
-  // componentDidMount() {
-  //   const url = `https://www.arcgis.com/sharing/rest/search?q=type%3Astorymap%20AND%20typekeywords%3A(smstatuspublished)%20AND%20orgid%3A(NOT(dGkWQRTbdsLW3GnN%20AND%204E2kXnaVf5dNC6X0%20AND%20nzS0F0zdNLvs7nc8))&num=100&f=pjson`;
-  //   fetch(url).then(
-  //     data => {
-  //       console.log(data);
-  //       this.setState({
-  //         isLoaded: true,
-  //         //current: data
-  //         items: data.items
-  //       });
-  //     },
-  //     error => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         error
-  //       });
-  //     }
-  //   );
-  // }
-
   render() {
-    const { error, isLoaded, results } = this.state;
+    const { error, isLoaded, items } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
+      console.log(items);
       return (
         <div className="App">
           <h1>Hello, Sara</h1>
           <ul>
-            {results.map(item => (
+            {items.map(item => (
               <li key={item.id}>
-                {item.title} {item.author} {item.description}
+                <ol>{item.thumbnail}</ol>
+                <ol>{item.title}</ol>
+                <ol>{item.author}</ol>
+                <ol>{item.description}</ol>
               </li>
             ))}
           </ul>
