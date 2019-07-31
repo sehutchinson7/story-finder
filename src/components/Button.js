76193f058118f61b772478f4a5bd6ea2b0eae70c
Sara <sehutchinson7@gmail.com>
2019-07-31 13:53:07 -0700

    Add flex to .card-body, styling changes to anchor button to bottom  of card

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,17 +6,19 @@ class StoryButton extends React.Component {
   render() {
     //TODO Replace button color with #00bab5 for branding
     return (
-      <Button
-        href={this.props.href}
-        tag="a"
-        className="btn-btn-info"
-        role="button"
-        alt="story url"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        {this.props.children}
-      </Button>
+      <div className="button-center">
+        <Button
+          href={this.props.href}
+          tag="a"
+          className="btn-btn-info"
+          role="button"
+          alt="story url"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {this.props.children}
+        </Button>
+      </div>
     );
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,17 +6,17 @@ import "./Card.css";
 class StoryCard extends React.Component {
   render() {
     return (
-      // <div className="grid-container">
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={this.props.src} alt="thumbnail" />
         <Card.Body>
-          <Card.Title>{this.props.title}</Card.Title>
-          <p>{this.props.owner}</p>
-          <Card.Text>{this.props.snippet}</Card.Text>
+          <div>
+            <Card.Title>{this.props.title}</Card.Title>
+            <p>{this.props.owner}</p>
+            <Card.Text>{this.props.snippet}</Card.Text>
+          </div>
           <StoryButton href={this.props.href}>Read story</StoryButton>
         </Card.Body>
       </Card>
-      // </div>
     );
   }
 }
