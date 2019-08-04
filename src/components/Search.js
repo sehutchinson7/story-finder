d34c4d258be3bdbf5d5752c2058c8dbd1b54dda3
sehutchinson7 <sehutchinson7@gmail.com>
2019-08-03 20:44:32 -0700

    Styling updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,14 +4,38 @@ import "./Search.css";
 function SearchBox(props) {
   console.log(props);
   return (
-    <input
-      value={props.query}
-      placeholder="Search stories"
-      onChange={props.handleSearchChange}
-      autoComplete="on"
-    />
+    <div>
+      <span class="icon-search">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+          <path d="M28.996 28.29l-9.454-9.456.268-.268a9.5 9.5 0 1 0-1.244 1.244l.268-.268 9.455 9.454zM6.42 18.581a8.6 8.6 0 1 1 12.162 0 8.6 8.6 0 0 1-12.162 0z" />
+        </svg>
+        <input
+          value={props.query}
+          placeholder="Search stories"
+          onChange={props.handleSearchChange}
+          autoComplete="on"
+        />
+      </span>
+    </div>
   );
 }
+// function SearchBox(props) {
+//   console.log(props);
+//   return (
+//     <div className="wrapper">
+//       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+//         <path d="M28.996 28.29l-9.454-9.456.268-.268a9.5 9.5 0 1 0-1.244 1.244l.268-.268 9.455 9.454zM6.42 18.581a8.6 8.6 0 1 1 12.162 0 8.6 8.6 0 0 1-12.162 0z" />
+//       </svg>
+
+//       <input
+//         value={props.query}
+//         placeholder="Search stories"
+//         onChange={props.handleSearchChange}
+//         autoComplete="on"
+//       />
+//     </div>
+//   );
+// }
 
 export default SearchBox;
 // let SearchBox = React.createClass({
